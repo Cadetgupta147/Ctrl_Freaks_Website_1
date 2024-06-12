f6a2637e2c598928f89f9ab245d4cb2d72b61269
Sagar Gupta <sg090186@gmail.com>
2024-06-13 03:00:11 +0530

    Adding shoes reveal effect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var trending_nav = $(".nav_link a");
 var featured_items = $(".featured_item");
+var focus = $("#focus");
 $(".nav_link").eq(0).css("background-color","red");
 trending_nav.on("click",function(){
     var card_id = $(this).html();
@@ -14,4 +15,19 @@ trending_nav.on("click",function(){
         }
     };
 });
-Shery.imageEffect(".card_img" ,{style:5, debug:true, gooey:true});
+var mensCard = $(".mens_card")[0]; // Get the raw DOM element
+var focus = $("#focus");
+
+$(window).on("mousemove", function(e) {
+    // Get mouse position relative to .mens_card
+    var rect = mensCard.getBoundingClientRect();
+    var mouseX = e.clientX - rect.left;
+    var mouseY = e.clientY - rect.top;
+
+    focus.css({
+        left: mouseX + "px",
+        top: mouseY + "px"
+    });
+});
+
+// Shery.imageEffect("" ,{style:5, debug:true, gooey:true});
