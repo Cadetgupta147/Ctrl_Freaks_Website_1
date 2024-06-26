043f36e8d0d7d93896e44ff9081403c7d8b0a548
Azeem <mazeem.ajm@gmail.com>
2024-06-26 16:47:21 +0530

    worked on firt section make the hovering transition better under shop for category

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ trending_nav.on("click",function(){
         }
     };
 });
+const handleOnMouseMove = e => {
+    const {currentTarget: target} = e;
+
+    const rect = target.getBoundinfClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+    target.style.setProperty("--mouse-x", `${x}px`)
+    target.style.setProperty("--mouse-y", `${y}px`)
+}
